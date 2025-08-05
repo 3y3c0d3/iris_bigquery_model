b6b063c7336d4895ee022cfee5e812855057d1e3
3y3c0d3 <97314133+3y3c0d3@users.noreply.github.com>
2025-08-05 17:05:06 -0500

    Update evaluate.sql

--- queries/evaluate.sql
+++ queries/evaluate.sql
@@ -1,2 +1,7 @@
 SELECT * 
 FROM ML.EVALUATE(MODEL `endless-ripple-465615-b4.iris_ml.iris_model`);
+
+##RESULTS
+
+	precision	                recall	             accuracy	           f1_score	          log_loss	         roc_auc
+	0.97382478632478631	0.97333333333333327	0.97333333333333327	0.97332266239829268	0.11736898115989577	0.99924142524142523
