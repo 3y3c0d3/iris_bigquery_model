32a3f081451c73c2f217b5613067b82afc75413b
3y3c0d3 <97314133+3y3c0d3@users.noreply.github.com>
2025-08-05 16:59:38 -0500

    Update create_model.sql

--- queries/create_model.sql
+++ queries/create_model.sql
@@ -1,1 +1,12 @@
-model creation sql here
+CREATE OR REPLACE MODEL `endless-ripple-465615-b4.iris_ml.iris_model` 
+OPTIONS(
+  model_type='logistic_reg'
+  , input_label_cols=['species']
+) AS
+SELECT
+  sepal_length
+, sepal_width
+, petal_width
+, petal_length
+, species
+FROM `bigquery-public-data.ml_datasets.iris`;
